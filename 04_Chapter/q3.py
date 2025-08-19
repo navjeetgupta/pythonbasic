cd1a6bf4702a49dc708e63dbcdfbd5667eef5fc6
navjeetgupta <navjeetguptayt@gmail.com>
2025-08-19 09:37:08 +0530

    chapter 4

--- 04_Chapter/q3.py
+++ 04_Chapter/q3.py
@@ -1,2 +1,9 @@
 #Check that a tuple type cannot be changed in python
 
+manu= ("manu", 34, 47)
+# print(type(manu))
+manu[1]=12
+print(manu) 
+
+# run and you will get to know tuple can't be changed.
+# it is immutable
---
+++ 04_Chapter/q4.py
@@ -0,0 +1,4 @@
+# program to sum a list with 4 numbers.
+x=[12, 13, 14, 15]
+print(sum(x)) 
+#sum is the builtin in function to sum.
